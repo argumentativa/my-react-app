4bbb3d79bf11adc9b9dc7420166fe7233facac6e
Miguel Arias <miguela@zg00044.zillow.local>
2019-05-17 14:18:12 -0700

    syncing animations and responsive layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,13 +7,16 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>TXL3R Design System</h1>
-    <p>The Trulia Experience Language (TXL) is based on sound design principles with the goal of developing a single unified experience across our platforms. TXL is a modular based design system allowing the team to scale and envolve designs while still adapting to different user needs. The module approach allows us to reuse much of our library in other parts of the experience and across multiple teams allow multiple teams to work concurrently and still maintain quality.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="Hero">
+      <div className="HeroGroup">
+
+      <h1>TXL3R<br />Design System</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+      <Link to="/page-2/">Watch Video</Link>
+
+      </div>
     </div>
-    <Link to="/page-2/">Watch Video</Link>
+
   </Layout>
 )
 
