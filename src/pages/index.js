9b4a4e30b6c0a04bcb487c3716427d0af3d4920f
Miguel Arias <miguela@zg00044.zillow.local>
2019-05-17 11:22:01 -0700

    watch button text

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>TXL3R Design System</h1>
+    <p>The Trulia Experience Language (TXL) is based on sound design principles with the goal of developing a single unified experience across our platforms. TXL is a modular based design system allowing the team to scale and envolve designs while still adapting to different user needs. The module approach allows us to reuse much of our library in other parts of the experience and across multiple teams allow multiple teams to work concurrently and still maintain quality.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Watch Video/Link>
   </Layout>
 )
 
